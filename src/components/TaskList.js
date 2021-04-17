f840999419c8822fbc05eb41a83923e7b565a8ea
Kevin Candelaria <kevdashdev@gmail.com>
2021-04-17 13:42:52 +0800

    Add redux

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Task from './Task';
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../lib/redux';
 
-export default function TaskList({
+export function PureTaskList({
   loading,
   onArchiveTask,
   onPinTask,
@@ -53,13 +55,22 @@ export default function TaskList({
   )
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   loading: PropTypes.bool,
   onArchiveTask: PropTypes.func,
   onPinTask: PropTypes.func,
   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired
 }
 
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
   loading: false
 }
+
+export default connect(
+  ({tasks}) => ({
+    tasks: tasks.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'),
+  }), dispatch => ({
+    onArchiveTask: id => dispatch(archiveTask(id)),
+    onPinTask: id => dispatch(pinTask(id))
+  })
+)
